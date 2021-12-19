6748a9836232a0c9a8feb730625dcb4082272525
Damian <dgr.wroc@gmail.com>
2021-12-19 21:19:11 +0100

    App, LearningResources

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in storedresources"
+      :key="resource.id"
+      :id="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedresources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
