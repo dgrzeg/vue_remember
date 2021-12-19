b4080ff32ce8c23afb9695a3ae15533fc3dcf02c
Damian <dgr.wroc@gmail.com>
2021-12-19 22:36:41 +0100

    AddResource,TheResources

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
   <TheHeader title="RememberMe" />
-  <StoredResources :resources="storedresources" />
+  <TheResources />
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
+    TheResources,
     TheHeader,
-  },
-  data() {
-    return {
-      storedresources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google',
-          link: 'https://google.com',
-        },
-      ],
-    };
   },
 };
 </script>
